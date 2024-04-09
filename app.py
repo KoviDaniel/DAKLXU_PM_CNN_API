8bf746addae3fa4e758602a68b60f6aea4074bc4
kovacs.daniel@stud.uni-obuda.hu <kovacs.daniel@stud.uni-obuda.hu>
2024-04-09 11:56:33 +0200

    using gray pictures, model might need to be modified

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ from PIL import Image
 from skimage import filters, restoration, exposure
 import numpy as np
 import os
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
 # Betöltjük a mentett modellt
-model = load_model('C:\CNN_model\model.h5')
+#model = load_model('C:\CNN_model\model.h5')
+model = load_model('C:\CNN_model\KDmodel3.h5')
 
 classes = ['Acer_campestre','Acer_platanoides','Acer_tataricum',
            'Alnus_glutinosa', 'Alnus_incana', 'Betula_pendula',
@@ -23,12 +25,27 @@ classes = ['Acer_campestre','Acer_platanoides','Acer_tataricum',
 
 # Kép előfeldolgozása a modell számára
 def preprocess_image(image_path):
-    # img = image.load_img(image_path, target_size=(224, 224))  # A modell VGG-16-hoz 224x224-es méretű képet vár
-    # img_array = image.img_to_array(img)
-    # img_array = np.expand_dims(img_array, axis=0)
-    # return img_array
+    img = image.load_img(image_path, target_size=(224, 224))  # A modell VGG-16-hoz 224x224-es méretű képet vár
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    return img_array
+    # img = Image.open(image_path).convert('L')
+    # img = img.resize((224, 224))
+    # img_array = np.array(img)
+    # threshold = filters.threshold_otsu(img_array)
+    # binary_img = img_array > threshold
+    # denoised_img = restoration.denoise_tv_chambolle(binary_img, weight=0.1)
+    # p2, p98 = np.percentile(img_array, (2, 98))
+    # img_rescale = exposure.rescale_intensity(img_array, in_range=(p2, p98))
+    # inverted_img = np.invert(img_rescale)
+    # edges = filters.sobel(inverted_img)
+    
+    # # Az előfeldolgozott kép visszaadása a modell számára
+    # processed_img_array = np.expand_dims(edges, axis=0)
+    # return processed_img_array
+
+def transform_image(image_path):
     img = Image.open(image_path).convert('L')
-    img = img.resize((224, 224))
     img_array = np.array(img)
     threshold = filters.threshold_otsu(img_array)
     binary_img = img_array > threshold
@@ -37,10 +54,9 @@ def preprocess_image(image_path):
     img_rescale = exposure.rescale_intensity(img_array, in_range=(p2, p98))
     inverted_img = np.invert(img_rescale)
     edges = filters.sobel(inverted_img)
-    
-    # Az előfeldolgozott kép visszaadása a modell számára
-    processed_img_array = np.expand_dims(edges, axis=0)
-    return processed_img_array
+    inverted_edges =  1 - edges
+    enhanced_edges = inverted_edges > 0.93
+    plt.imsave('transformed_image.jpg', enhanced_edges, cmap='gray')
 
 # API végpont a képek fogadására és predikciójának visszaadására
 @app.route('/predict', methods=['POST'])
@@ -54,9 +70,11 @@ def predict():
 
     try:
         img_path = 'temp_image.jpg'
+        tr_img_path = 'transformed_image.jpg'
         file.save(img_path)
 
-        processed_image = preprocess_image(img_path)
+        transform_image(img_path)
+        processed_image = preprocess_image(tr_img_path)
         prediction = model.predict(processed_image)
 
         max_prob_index = np.argmax(prediction)
@@ -65,10 +83,13 @@ def predict():
             'PredictedClass': predicted_class,
         }
         os.remove(img_path)
+        os.remove(tr_img_path)
 
         return jsonify(result)
 
     except Exception as e:
+        os.remove(img_path)
+        os.remove(tr_img_path)
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
